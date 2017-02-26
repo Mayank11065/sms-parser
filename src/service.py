4862f4d6ce9d0090ca7007eeaff1189a15924db3
Mayank11065 <mayank.garg1805@gmail.com>
2017-02-26 18:53:27 +0530

    Adding config files

--- src/service.py
+++ src/service.py
@@ -2,13 +2,20 @@
 """Simple HTTP server"""
 
 from http.server import BaseHTTPRequestHandler
-
+from smsDataHandler import SmsHandler
 # HTTPRequestHandler class
+sms = SmsHandler()
 
 class TestHTTPServerRequestHandler(BaseHTTPRequestHandler):
     """HTTP server Class"""
+    #def __init__(self):
+    #    sms = SmsHandler()
+
     def do_GET(self):
         """GET Function"""
+
+        if self.path == '/sms':
+            sms.getSms(self)
         # Send response status code
         self.send_response(200)
 
---
+++ src/smsDataHandler.py
@@ -0,0 +1,9 @@
+"""SMS parser"""
+
+class SmsHandler:
+    """SMS Handler"""
+    def __init__(self):
+        pass
+    def getSms(self, request):
+        """Receive SMS Data"""
+        print("Reached here")
